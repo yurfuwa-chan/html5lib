e55a63358c1e8fa998e211ac302435e3d969ca32
kaede <AdobeCreativeSuite3@gmail.com>
2011-03-16 18:05:59 +0900

    dnd rewrittable : localimage draggabule

--- script/dd.js
+++ script/dd.js
@@ -17,20 +17,51 @@ function dragOver(e){
 
 function drop(e){
     e.preventDefault();
-    var dt = e.dataTransfer
+
+    var dt = e.dataTransfer;
+    var files = dt.files;
     var url = dt.getData("URL");
-    var node;
-    if(url){
-       node = document.createElement("img");
-       node.src = url;
+    var str = dt.getData("text/plain");
+    
+    if(files.length){
+        addLocalImage(files);
     }else{
-       node = document.createTextNode(dt.getData("text/plain"));
+        if(url){
+            addImage(url);
+        }else if(str){
+            addText(str);
+        }
     }
-    if(node){
-        var p = document.createElement("p");
-        p.appendChild(node);
-        dropArea.appendChild(p);
+}
+
+function addLocalImage(files){
+    var file = files[0];
+    //FileType”»’è
+    if(file.type.match(/image.*/)){
+        var reader = new FileReader();
+        reader.onloadend = readerLoadEnd;
+        reader.readAsDataURL(file);
     }
+
+    function readerLoadEnd(e){
+        addImage(e.target.result);
+    }
+}
+
+function add(node){
+    var p = document.createElement("p");
+    p.appendChild(node);
+    dropArea.appendChild(p);
+}
+
+function addText(str){
+    add(document.createTextNode(str));
+}
+
+function addImage(src){
+    var img = document.createElement("img");
+    img.src = src;
+    add(img)
 }
 
 window.addEventListener("load",init,false);
