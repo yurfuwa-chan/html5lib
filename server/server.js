2b27e16af4bc136dae987d972be654f505f6d67d
kaede <AdobeCreativeSuite3@gmail.com>
2011-03-18 23:38:53 +0900

    server.js : createServer

--- server/server.js
+++ server/server.js
@@ -6,11 +6,7 @@
 var http = require('http'),
 io = require('socket.io'), // for npm, otherwise use require('./path/to/socket.io') 
 
-server = http.createServer(function(req, res){
- // your normal server code
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('<h1>Hello world</h1>');
-});
+server = http.createServer();
 
 server.listen(3000);
 
@@ -19,7 +15,7 @@ console.log("Server running at http://127.0.0.1:3000/");
 // socket.io
 var socket = io.listen(server);
 socket.on('connection', function(client){
-    client.send("wellcome new user");
+  client.send("wellcome new user");
   client.on('message', function(data){
     client.send(data);
     client.broadcast(data);
